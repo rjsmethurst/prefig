a1dc0ffd890522ec99bfb43bbfcd6566fd80e241
rjsmethurst <rebecca.smethurst@astro.ox.ac.uk>
2014-06-25 11:55:51 +0100

    testing function, need to add colour inversion

--- prefig.py
+++ prefig.py
@@ -19,29 +19,35 @@ class Prefig(plt.Figure):
         :fontcol:
             The colour of the axes labels and tick labels of the plot (optional). Default is 'black' or 'k'.
         
-        :rot: 
-            Rotation angle of axes tick labels of the plot (optional). Default is 45 degrees.
-        
         :figsize:
             tuple of integers (optional). (width, height) in inches. Default is (16,12).
         :font: 
             font family (optional). Default from rc.font file. Can be specified within plot commands instead. 
         
         """
-    def __init__(self, axcol='k', fontcol='k', rot=45, size=(16,12), font='serif'):
+    def __init__(self, axcol='k', fontcol='k', size=(16,12), font='serif'):
         self.axcol = axcol
         self.fontcol = fontcol
-        self.rot = rot
         self.size = size
         self.font = font
-        plt.rc('figure', figsize=size, facecolor='none'), edgecolor='none')
+        
+        #if axcol='white' or 'w':
+        #access all colours called in plt functions
+        #convert to rgb string
+        #invert rgb string
+        #set colours to inverted rgb string
+        
+        #
+        
+        plt.rc('figure', figsize=size, facecolor='none', edgecolor='none')
         plt.rc('savefig', dpi=300, facecolor='none', edgecolor='none', frameon='False')
-        f = {'family':font}
+        f = {'family':font, 'size':20}
         plt.rc('font', **f)
         plt.rc('text', color=fontcol)
         plt.rc('axes', labelsize='x-large', edgecolor=axcol, labelcolor=fontcol, facecolor='none')
         plt.rc('xtick', labelsize='large', color=fontcol)
-        plt.rc('yrick', labelsize='large', color=fontcol)
+        plt.rc('ytick', labelsize='large', color=fontcol)
+        plt.rc('lines', markersize=10, linewidth=2)
         
 
 
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from prefig import Prefig
 
 x = np.linspace(8,10,50)+ np.random.normal(0,0.5, 50)
 m, c = 1.5, -3
 y = m*x + c + np.random.normal(0,0.5,50)
-yerr = N.random.normal(0,0.1,50)
+yerr = np.random.normal(0,0.1,50)
 
-P.figure()
-P.errorbar(x,y, yerr, xerr=none, ecolor='white', c='white', marker='D')
-P.xlabel('measured')
-P.ylabel('observed')
-P.savefig('test.png')
+Prefig(axcol='w', fontcol='w')
+plt.errorbar(x,y, yerr, xerr=None, fmt=' ', ecolor='white', c='white', marker='D', mec='white')
+plt.xlabel('measured')
+plt.ylabel('observed')
+plt.savefig('test.png')
